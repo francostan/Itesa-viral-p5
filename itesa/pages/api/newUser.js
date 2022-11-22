a3813e2ff7d67df84d5dfa49c4e60d14e1de7428
Bautistagl <bautistagonzalezlazo@gmail.com>
2022-11-22 18:49:27 -0300

    Logica 2FA

--- itesa/pages/api/newUser.js
+++ itesa/pages/api/newUser.js
@@ -1,17 +1,24 @@
 const db = require("../../db/models/index");
 const User = db.User;
+const speakeasy = require("speakeasy");
 
-export default async function newuser (req, res) {
+export default async function newuser(req, res) {
   const { method, body } = req;
+
   switch (method) {
-    case "POST":{
-        const created = await User.create(body)
-        res.json("Usuario creado con éxito")}
+    case "POST":
+      {
+        const created = await User.create(body);
+        res.json(created);
+      }
       break;
-    case "GET":{
-      res.json("Llegaste a GET")}
+    case "GET":
+      {
+        res.json("Llegaste a GET");
+      }
       break;
-    default:res.send("Otro método")
+    default:
+      res.send("Otro método");
       break;
   }
-};
+}
