ba1d07e34f830799b4db86f6044c7e76ab4a4093
Franco Stanziola <92806134+francostan@users.noreply.github.com>
2022-11-23 18:07:40 -0300

    home view ready / gitignore

--- itesa/pages/home.js
+++ itesa/pages/home.js
@@ -6,14 +6,23 @@ import {
   Heading,
   HStack,
   Image,
+  GridItem,
   Text,
   VStack,
   Flex,
+  Box,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
 const Home = () => {
   //<Heading fontFamily={"poppins"} color={"white"} fontSize={"60px"} >Index</Heading>
+ /*  <Image
+        src="https://media.discordapp.net/attachments/1040681301201666089/1045049523577299075/Screen_Shot_2021-08-24_at_17.58.36.png?width=386&height=414"
+        alt="logo"
+        objectFit="cover"
+        boxSize="300px"
+        borderRadius={"20%"}
+      /> */
 
   return (
     <Grid
@@ -35,14 +44,21 @@ const Home = () => {
           <Heading color="#9d39fe">Coin</Heading>
         </HStack>
       </HStack>
-      <Image
-        src="https://media.discordapp.net/attachments/1040681301201666089/1045049523577299075/Screen_Shot_2021-08-24_at_17.58.36.png?width=386&height=414"
-        alt="logo"
-        objectFit="cover"
-        boxSize="300px"
-        borderRadius={"20%"}
-      />
-
+      <GridItem
+      h={"60vh"}
+      w={"50vw"}
+      align={"center"}
+      bgRepeat="no-repeat"
+      bgPosition={"center"}
+      bgSize={"auto"}
+      bgImage={"https://media.discordapp.net/attachments/1040681301201666089/1045049523577299075/Screen_Shot_2021-08-24_at_17.58.36.png?width=386&height=414"}
+      >
+        <Flex direction={"column"} alignItems={"center"} justify={"center"} h={"100%"}>
+          <Text color={"white"}>Unite a nuestro universo</Text>
+          < Text color={"white"}>de criptomonedas</Text>
+        </Flex>
+      </GridItem>
+        
       <HStack
         w={"100%"}
         h={"100%"}
