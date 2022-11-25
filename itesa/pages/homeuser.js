4617996a4cd1e75038c2083f7113c00ab0ce9232
francostan <francostan98@hotmail.com>
2022-11-25 12:55:59 -0300

    persistence

--- itesa/pages/homeuser.js
+++ itesa/pages/homeuser.js
@@ -1,9 +1,13 @@
 import React from "react";
 import WalletCard from "../components/WalletCard";
+import Persistence from "../components/Persistence";
+import { useSelector } from "react-redux";
 
 const HomeUser = () => {
+  
   return (
     <div>
+      <Persistence />
       <WalletCard />
     </div>
   );
