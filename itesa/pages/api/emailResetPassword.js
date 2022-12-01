a93d87c1302743daedbb2e3a3631295c9140b6ed
Martin Hofmann <martinhofmann@Martins-MacBook-Pro.local>
2022-12-01 19:19:00 -0300

    Email sent to user to change password

---
+++ itesa/pages/api/emailResetPassword.js
@@ -0,0 +1,58 @@
+const db = require("../../db/models/index");
+const User = db.User;
+const nodemailer = require("nodemailer");
+let handlebars = require("handlebars");
+const fs = require("fs");
+
+function emailResetPassword(req, res) {
+  const { method, body } = req;
+  switch (method) {
+    case "POST": {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "itesa.getViral@gmail.com",
+          pass: "rtspkviskcrhorey",
+        },
+      });
+
+      fs.readFile(process.cwd() + "/views/mail.html", "utf-8", function (
+        err,
+        html
+      ) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        let template = handlebars.compile(html);
+        let replacements = {
+          email: body.email,
+        };
+        let htmlToSend = template(replacements);
+        let mailOptions = {
+          from: "GetViral",
+          to: body.email,
+          subject: "Recuperar Contraseña",
+          html: htmlToSend,
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log("Error de mail");
+            console.log(error.message);
+            res.status(404).send();
+            //.send(error.message);
+          } else {
+            res.status(200).send({
+              email: null,
+              nick_name: null,
+              id: usuario.dataValues.id,
+            });
+          }
+        });
+        res.status(202).send();
+      });
+    }
+  }
+}
+
+export default emailResetPassword;
