eb4065ecf8b8c45e13a382a978b1b89b549fc6a8
Szayek17 <santiago.ph17@hotmail.com>
2022-12-01 22:42:50 -0300

    new ranking

---
+++ itesa/commons/RankUsers.js
@@ -0,0 +1,135 @@
+import React from "react";
+import {
+  Flex,
+  Box,
+  Text,
+  Image,
+  Divider,
+  Grid,
+  HStack,
+  List,
+  ListItem,
+  Link,
+} from "@chakra-ui/react";
+
+export default function ListTop3({ ranking }) {
+  //Single ranked va a venir como un objeto con un id(refferingId) y una cantidad de invitados
+  // ranking = [{referringId: 1, awards: 19, referringId: 2, awards: 18, referringId: 3, awards: 17}]
+
+  const rankingVisual = [
+    { img: "/gold.png", alt: "Primer puesto" },
+    { img: "/silver.png", alt: "Segundo puesto" },
+    { img: "/bronze.png", alt: "Tercer puesto" },
+  ];
+  return (
+    <Grid
+      alignContent={"flex-start"}
+      color="white"
+      bgGradient="linear(black,#9d39fe)"
+      h="100vh"
+    >
+      <Flex w={"100%"} direction="column" justify={"flex-start"} p={4}>
+        <HStack spacing={12}>
+          <Text
+            textAlign="center"
+            as={"h1"}
+            fontSize={"6xl"}
+            fontWeight={"bold"}
+          >
+            {" "}
+            Ranking
+          </Text>
+          <Link href="/logged/homeuser">
+            <Image
+              src="/banana.png"
+              alt="logo"
+              border="1px"
+              boxSize="50px"
+              borderRadius={"20%"}
+            />
+          </Link>
+        </HStack>
+        {ranking?.slice(0, 3).map((puesto, index) => (
+          <Flex align={"center"} direction={"column"} mx={"2"}>
+            <Flex direction={"row"} marginTop="4">
+              <Image
+                src={rankingVisual[index].img}
+                alt={rankingVisual[index].alt}
+                width={"35%"}
+              />
+              <Flex direction={"column"} justify={"space-around"}>
+                <Box mx={"4"}>
+                  <Text as={"h2"} fontSize={"4xl"} fontWeight={"bold"} mb={"2"}>
+                    {puesto.nick_name}
+                  </Text>
+                  <Text as={"h3"} fontSize={"lg"} fontWeight={"light"}>
+                    Total de invitados: {puesto.awards}
+                  </Text>
+                </Box>
+              </Flex>
+            </Flex>
+          </Flex>
+        ))}
+        <List direction={"column"} paddingTop="5" h="100%">
+          {ranking?.length > 3 &&
+            ranking?.slice(3).map((item, index) => (
+              <ListItem>
+                <Divider
+                  p={1}
+                  orientation="horizontal"
+                  borderColor={"black"}
+                  my={4}
+                />
+
+                <Text
+                  fontWeight="bold"
+                  fontSize="2xl"
+                  textAlign="center"
+                  marginBottom={1}
+                >
+                  Ranking: {index + 4}
+                </Text>
+                <Flex
+                  direction={"column"}
+                  align="center"
+                  justifyContent={"center"}
+                >
+                  <Text marginTop={1} marginBottom={1}>
+                    Nombre: {item.nick_name}
+                  </Text>
+
+                  <Text marginTop={1} marginBottom={1}>
+                    Total de invitados: {item.awards}{" "}
+                  </Text>
+                </Flex>
+              </ListItem>
+            ))}
+        </List>
+        {/* <Flex align={"center"} mx={"2"}>
+          <Image src="/silver.png" width={"25%"} />
+          <Box mx={"4"}>
+            <Text as={"h2"} fontSize={"2xl"} fontWeight={"bold"} mb={"2"}>
+              {top3[1].nick_name}
+            </Text>
+            <Text as={"h3"} fontSize={"lg"} fontWeight={"light"}>
+              Total de invitados : {top3[1].awards}
+            </Text>
+          </Box>
+        </Flex>
+        <Divider orientation="horizontal" borderColor={"black"} my={"2"} />
+        <Flex align={"center"} mx={"2"}>
+          <Image src="/bronze.png" alt="Tercer Puesto" width={"20%"} />
+          <Box mx={"4"}>
+            <Text as={"h2"} fontSize={"lg"} fontWeight={"bold"} mb={"2"}>
+              {top3[2].nick_name}
+            </Text>
+            <Text as={"h3"} fontSize={"lg"} fontWeight={"light"}>
+              Total de invitados :{top3[2].awards}
+            </Text>
+          </Box>
+        </Flex>
+        <Divider orientation="horizontal" borderColor={"black"} my={"2"} /> */}
+      </Flex>
+    </Grid>
+  );
+}
--- itesa/components/Ranking.js
+++ itesa/components/Ranking.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "../config/axios";
-import ListTop3 from "../commons/listTop3";
+import ListTop3 from "../commons/RankUsers";
 import { useState } from "react";
 import {
   Button,
@@ -8,13 +8,15 @@ import {
   List,
   ListItem,
   HStack,
-  Text
+  Text,
+  Flex,
+  Divider,
 } from "@chakra-ui/react";
 
 const Ranking = () => {
   const [ranking, setRanking] = React.useState([]);
   const [mostrar, setMostrar] = useState(false);
-  const [rankingBottom, setRangingBottom] = useState([]);
+  const [rankingBottom, setRankingBottom] = useState([]);
 
   React.useEffect(() => {
     axios.get("/ranking").then((response) => {
@@ -22,36 +24,14 @@ const Ranking = () => {
     });
   }, []);
 
-  const handleMostrar = () => {
-    const temp=ranking.slice(3,ranking.length)
-    setRangingBottom(temp);
-    setMostrar(!mostrar);
-  };
-  console.log(rankingBottom);
   return (
-    <Grid>
+    <Flex direction={"column"} w={400} h="100vh">
       {ranking.length > 0 ? (
-        <ListTop3 top3={ranking.slice(0, 3)} />
+        <ListTop3 ranking={ranking} />
       ) : (
         <p>No hay ranking disponible</p>
       )}
-      <Button onClick={handleMostrar}>Ver Ranking Completo</Button>
-      {mostrar ? (
-        <List spacing={3}>
-          {rankingBottom?.map((rank,i) => (
-            <ListItem>
-                <Text>{i+4}</Text>
-              <HStack spacing={2}>
-                <Text>Nick Name:{rank.nick_name}</Text>
-                <Text>Awards: {rank.awards}</Text>
-              </HStack>
-            </ListItem>
-          ))}
-        </List>
-      ) : (
-        <></>
-      )}
-    </Grid>
+    </Flex>
   );
 };
 
