5371c352396666c8e10c7673022fd2f9a20a3ff9
Martin Hofmann <martinhofmann@Martins-MacBook-Pro.local>
2022-12-01 21:14:34 -0300

    Added GET route

--- itesa/pages/api/resetPassword.js
+++ itesa/pages/api/resetPassword.js
@@ -3,10 +3,20 @@ const User = db.User;
 
 export default async function newuser(req, res) {
   const { method } = req;
+  const email = req.body.email;
   const id = req.body.id;
   const password = req.body.password;
 
+  console.log("REQQQQ BODYYYYY>>>>", req.body);
+
   switch (method) {
+    case "POST":
+      {
+        const user = await User.findOne({ where: { email } });
+        res.send(user);
+      }
+      break;
+
     case "PUT":
       {
         const user = await User.findByPk(id);
